99c977f6b54367266021b95cb1caf643967f0768
Connor Boyle <connor.boyle@lightsource.ca>
2021-06-17 17:18:53 -0600

    Use the correct index variable

--- uca-pcome4-camera.c
+++ uca-pcome4-camera.c
@@ -479,10 +479,8 @@ static gboolean uca_pco_me4_camera_grab(UcaCamera *camera, gpointer data, GError
             return FALSE;
         }
 
-        priv->last_image++;
-
         gpointer frame;
-        err = pco_get_image_ptr(priv->pco, &frame, priv->last_image);
+        err = pco_get_image_ptr(priv->pco, &frame, index);
         CHECK_AND_RETURN_VAL_ON_PCO_ERROR(err, FALSE);
 
         if (frame == NULL)
@@ -493,6 +491,7 @@ static gboolean uca_pco_me4_camera_grab(UcaCamera *camera, gpointer data, GError
 
         pco_extract_image(priv->pco, data, frame, priv->image_width, priv->image_height);
 
+        priv->last_image = index;
         return TRUE;
     }
 }
